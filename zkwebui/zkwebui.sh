fabb144687545b3fc6d54c2e50abdbef365d6fd2
dongliang <dongliang@suiyi.com.cn>
2019-09-26 10:23:38 +0800

    zkwebui

--- zkwebui/zkwebui.sh
+++ zkwebui/zkwebui.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#root@9ba5242c2068:/zk-web/conf# cat zk-web-conf.clj
+#{
+#:server-port 8080
+# :users {"admin" "hello"}
+# :default-node ""
+#}
+#可外置修改conf
+
 docker rm -f zkwebui
 
 docker run --restart=always --name zkwebui -d -p 6062:8080 vivint/zk-web-docker
